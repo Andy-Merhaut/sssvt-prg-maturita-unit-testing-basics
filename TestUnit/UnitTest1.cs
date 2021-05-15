83057caa993340fe5c6a933d2b63ab57de689516
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-15 17:30:52 +0200

    test body

--- TestUnit/UnitTest1.cs
+++ TestUnit/UnitTest1.cs
@@ -7,8 +7,32 @@ namespace TestUnit
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_AddMethod()
         {
+            BasicMaths basicMaths = new BasicMaths();
+            double result = basicMaths.Add(10, 10);
+            Assert.AreEqual(result, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths basicMaths = new BasicMaths();
+            double result = basicMaths.Deduct(10, 10);
+            Assert.AreEqual(result, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMaths basicMaths = new BasicMaths();
+            double result = basicMaths.Divide(10, 5);
+            Assert.AreEqual(result, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths basicMaths = new BasicMaths();
+            double result = basicMaths.Multiply(10, 10);
+            Assert.AreEqual(result, 100);
         }
     }
 }
