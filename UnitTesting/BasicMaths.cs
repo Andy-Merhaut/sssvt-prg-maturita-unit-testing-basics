48a1cd2d7a67d01b8d065cc4a623adca9a9c9d8f
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-15 18:16:02 +0200

    done

--- UnitTesting/BasicMaths.cs
+++ UnitTesting/BasicMaths.cs
@@ -10,17 +10,22 @@ namespace UnitTesting
         {
             return num1 + num2;
         }
+
         public double Deduct(double num1, double num2)
         {
             return num1 - num2;
         }
+
         public double Divide(double num1, double num2)
         {
             return num1 / num2;
         }
+
         public double Multiply(double num1, double num2)
         {
             // To trace error while testing, writing + operator instead of * operator.  
+            // Toto bude padat, abychom videli, ze testing funguje
+            // oprava je zmena z operatoru plus na operator nasobeni (hvezdicka)
             return num1 + num2;
         }
     }
