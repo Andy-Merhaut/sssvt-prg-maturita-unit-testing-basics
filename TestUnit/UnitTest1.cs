e95a62d4890198bd534cb5df20cde92bc55e928c
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-15 17:28:44 +0200

    Přidejte soubory projektu.

---
+++ TestUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTesting;
+
+namespace TestUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
